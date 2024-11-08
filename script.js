d329f39aa4751afef18d3e7b92e7b72bd700ce69
Ryan Smith <rcsmith92@yahoo.com>
2024-11-08 05:13:50 +0000

    added is and is else statements

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // all code for this lab to be completed in this file
+let currentTemperature = 40
+
+//2 possibilites: it's freezing or it's not
+if (currentTemperature > 32) {
+    console.log("It is not freezing.")
+} else {
+    console.log("It is freezing")
+}
+
+//3 possibilities: odler, younger, or same age
+// if...else if statement
+
+let myAge = 32
+let yourAge = 36
+
+if (myAge === yourAge) {
+    console.log("We are the same age!")
+}  else if (myAge > yourAge) {
+    console.log("I am older than you.")
+}  else if (myAge < yourAge) {
+    console.log("I am younger than you.")
+}  else {
+    console.log("Error! Try Again") 
+}
